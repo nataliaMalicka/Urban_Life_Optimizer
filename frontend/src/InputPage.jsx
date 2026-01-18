cda149e566d5b786b6b0f96602c7f7e5c335ebcb
Mu Ye Liu <muyeliu1208@gmail.com>
2026-01-17 19:58:49 -0800

    set up the useState for InputPage

--- frontend/src/InputPage.jsx
+++ frontend/src/InputPage.jsx
@@ -6,11 +6,15 @@ import Background from './assets/background.png'
 import HeaderComponent from "./HeaderComponent.jsx";
 
 function InputPage() {
+  const [gridPage, setGridPage] = useState(1);
+
   return (
     <>
       <img src={Background} class='background-image' />
-      <HeaderComponent />
-      <InputGrid />
+      <HeaderComponent/>
+      {gridPage == 1 && <InputGrid />}
+      {gridPage == 2 && <InputGrid2 />}
+      {gridPage == 3 && <InputGrid3 />}
     </>
   );
 }
