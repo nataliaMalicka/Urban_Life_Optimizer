7e79db56f9eb0aefc2b62f6184eedced82cd06d8
natalieM <nataliesemkow@gmail.com>
2026-01-17 19:43:08 -0800

    created input page 3

--- frontend/src/InputPage.jsx
+++ frontend/src/InputPage.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import "./InputPage.css";
 import "./InputGrid.css";
-import InputGrid from './InputGrid.jsx';
-import Background from './assets/background.png'
+import InputGrid3 from './InputGrid3.jsx';
+import Background from './assets/background.png';
 import HeaderComponent from "./HeaderComponent.jsx";
 
 function InputPage() {
-  return (
-    <>
-      <img src={Background} class='background-image' />
-      <HeaderComponent />
-      <InputGrid />
-    </>
-  );
+    const [gridPage, setGridPage] = useState(3);
+    const [formData, setFormData] = useState({});
+
+    return (
+        <>
+            <img src={Background} className='background-image' />
+            <HeaderComponent />
+
+            <InputGrid3
+                formData={formData}
+                setFormData={setFormData}
+                setGridPage={setGridPage}
+            />
+        </>
+    );
 }
 
 export default InputPage;
